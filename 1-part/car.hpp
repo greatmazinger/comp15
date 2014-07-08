ee5a32d18836e594102dfb416afb73a5c74021ca
Raoul Veroy <raoul@littletechtips.com>
2014-07-08 00:11:42 -0400

    Add part 1 of lecture.

---
+++ 1-part/car.hpp
@@ -0,0 +1,13 @@
+#ifndef CAR_HPP
+#define CAR_HPP
+
+#include <string>
+
+typedef struct 
+{
+    int year;
+    int mpg;
+    std::string make;
+} Car;
+
+#endif
---
+++ 1-part/teststack.cpp
@@ -0,0 +1,30 @@
+#include "stack.hpp"
+#include <cassert>
+#include <iostream>
+
+using std::cout;
+
+void print_car(Car c) {
+    cout << "A " << c.year << " " << c.make << " with " << c.mpg << " mpg." << std::endl; 
+}
+
+int main()
+{
+    // Test that making a giant stack doesn't cause problems 
+    cout << "Test giant stack." << std::endl;
+    Stack S;
+    Car c;
+    for (int i = 0; i < 1000000; ++i) {
+        S.push(c);
+    }
+
+    // Empty stack...what happens? (crash)
+    cout << "Test empty stack." << std::endl;
+    Stack S2;
+    c = S2.pop(); 
+    print_car(c); 
+
+    cout << "Tests passed." << std::endl;
+
+    return 0;
+}
